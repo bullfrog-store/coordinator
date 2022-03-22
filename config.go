4095dc0c98da7b330ec80e100262e7a6a4dcd983
liubinhao <liubinhao@momenta.ai>
2022-03-22 16:15:49 +0800

    feature: defined interface Peer and struct Modify

--- config.go
+++ config.go
@@ -3,8 +3,6 @@ package coordinator
 type Config struct {
 	// ID is the IP of current peer.
 	ID string
-	// Peers are the IPs of the cluster peers except me.
-	Peers []string
 	// ZKAddrs are the IPs of the ZK cluster.
 	ZKAddrs []string
 }
